ff9ce04212ba9f7a000f5930cdef96fcf9efc318
Kamrul Islam Touhid <kamrultouhids@gmail.com>
2018-02-25 23:53:21 +0600

    readme.md

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 
+use App\User;
+
+use App\Model\Patient;
+
 use Illuminate\Http\Request;
 
-use App\User;
-use App\Model\Patient;
 use App\Lib\Enumerations\RoleId;
+
 use App\Lib\Enumerations\UserStatus;
-
-
 
 
 class HomeController extends Controller
@@ -19,17 +20,16 @@ class HomeController extends Controller
 
     public function index()
     {
-        $total_physiotherapists=User::where('role_id' , RoleId::$PHYSIOTHERAPIST_ROLE)->get()->count();
-        $total_data_operator=User::where('role_id' , RoleId::$DATA_OPERATOR_ROLE)->get()->count();
-        $total_patient=Patient::where('status' , UserStatus::$ACTIVE)->get()->count();
+        $total_physiotherapists =   User::where('role_id' , RoleId::$PHYSIOTHERAPIST_ROLE)->get()->count();
+        $total_data_operator    =   User::where('role_id' , RoleId::$DATA_OPERATOR_ROLE)->get()->count();
+        $total_patient          =   Patient::where('status' , UserStatus::$ACTIVE)->get()->count();
 
-        $data=[
-
-            'physiotherapists'=>$total_physiotherapists,
-            'data_operator'=>$total_data_operator,
-            'patient'=>$total_patient,
-
+        $data = [
+            'physiotherapists'  => $total_physiotherapists,
+            'data_operator'     => $total_data_operator,
+            'patient'           => $total_patient,
         ];
+
         return view('admin.adminHome')->with($data);
     }
 
